afbf353d68acef57b359ca63396f79b2d5a7c642
djordje1620 <78233774+djordje1620@users.noreply.github.com>
2021-12-08 21:34:05 +0100

    update

--- js/main2.js
+++ js/main2.js
@@ -12,22 +12,58 @@ var year = date.getFullYear();
 var month = date.getMonth()+1;
 var todayDate = String(date.getDate()).padStart(2,'0');
 var datePattern = year + '-' + month + '-' + todayDate;
-document.getElementById("d_p").defaultValue = datePattern;
-document.getElementById("d_v").defaultValue = datePattern;
-// var x;
-// function ispisDatuma(){
-//    x =  document.getElementById("d_p").value;
-// }
-// if(x != undefine)
-// {
-//     console.log(x);
-// }
-// document.getElementById("d_pp").value = x;
+document.getElementById("d_p").value = datePattern;
+document.getElementById("d_v").value = datePattern;
 
-// function provera(){
-//     var vreme = document.getElementById("vr_preuzimanja");
-//     var x = vreme.options[vreme.selectedIndex].text;
-//     var y = vreme.options[vreme.selectedIndex].value;
-//     console.log("Text svojstvo ima vrednost: " + x);
-//     console.log("Value svojstvo ima vrednost: " + y);
+function upisDatuma() {
+    var d_p = document.getElementById("d_p").value;
+    localStorage.setItem("d_preuzimanja", d_p);
+    var d_v = document.getElementById("d_v").value;
+    localStorage.setItem("d_vracanja", d_v);
+    var v_p = document.getElementById("vremePreuzimanja").value;
+    localStorage.setItem("v_preuzimanja", v_p);
+    var v_v = document.getElementById("vremeVracanja").value;
+    localStorage.setItem("v_vracanja", v_v);
+
+    var vozila_lab = document.getElementById("lab_js");
+    vozila_lab.classList.add("boja_lab");
+}
+function onl(){
+    var d_pr = localStorage.getItem("d_preuzimanja");
+    document.getElementById("d_pp").value = d_pr;
+    localStorage.removeItem("d_preuzimanja");
+
+    var d_vr = localStorage.getItem("d_vracanja");
+    document.getElementById("d_vv").value = d_vr;
+    localStorage.removeItem("d_vracanja");
+
+    var v_pr = localStorage.getItem("v_preuzimanja");
+    document.getElementById("vremePreuzimanja2").value = v_pr;
+    localStorage.removeItem("v_preuzimanja");
+
+    var v_vr = localStorage.getItem("v_vracanja");
+    document.getElementById("vremeVracanja2").value = v_vr;
+    localStorage.removeItem("v_vracanja");
+    var vozila_lab = document.getElementById("lab_js");
+    vozila_lab.classList.add("boja_lab");
+}    
+
+// function removLocalStorage(){
+    
 // }
+// function readValue() {
+//     var x = localStorage.getItem("mytime");
+//     document.getElementById("demo").innerHTML = x;
+// }
+      
+// function ispisDatuma(){
+//     localStorage.setItem("vreme_preuzimanja", x);
+//     alert(x);
+// }
+// if(localStorage){
+    
+// }
+
+
+// var d_p = document.getElementById("d_pp");
+// d_p.value = x;
